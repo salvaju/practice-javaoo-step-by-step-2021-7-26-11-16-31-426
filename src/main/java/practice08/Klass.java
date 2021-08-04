c05dbe0cd2421f497f6263e9c9c28a3924b478b9
Juan Carlos Salvador <salvaju@oocl.com>
2021-08-04 20:41:04 +0800

    [practice08][Juan Carlos Salvador] feat: should_person_have_id_name_and_age

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,12 @@
 package practice08;
 
 public class Klass {
+
+    private int number;
+    private Student student;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
 }
--- src/main/java/practice08/Person.java
+++ src/main/java/practice08/Person.java
@@ -1,4 +1,23 @@
 package practice08;
 
 public class Person {
+
+    private String name;
+    private int age;
+    private int id;
+
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
 }
