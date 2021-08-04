40096acdad2be296f75a3af2dc1dedaa4b832185
Juan Carlos Salvador <salvaju@oocl.com>
2021-08-04 18:35:12 +0800

    [practice05][Juan Carlos Salvador] feat: should_teacher_introduce_itself_with_which_class_it_teaches

--- src/main/java/practice05/Teacher.java
+++ src/main/java/practice05/Teacher.java
@@ -1,5 +1,7 @@
 package practice05;
 
+import java.text.MessageFormat;
+
 public class Teacher extends Person{
 
     private Integer klass;
@@ -11,5 +13,10 @@ public class Teacher extends Person{
 
     public Integer getKlass() {
         return klass;
+    }
+
+    @Override
+    public String introduce(){
+        return MessageFormat.format("My name is {0}. I am {1} years old. I am a Teacher. I teach Class {2}.", getName(), getAge(), klass);
     }
 }
