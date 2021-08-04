644939dcf6104b12d52d369ca4c0189c3aec8939
Juan Carlos Salvador <salvaju@oocl.com>
2021-08-04 18:23:51 +0800

    [practice05][Juan Carlos Salvador] feat: should_student_have_name_age_and_class_number

--- src/main/java/practice05/Student.java
+++ src/main/java/practice05/Student.java
@@ -1,4 +1,16 @@
 package practice05;
 
-public class Student {
+public class Student extends Person{
+
+    private int klass;
+
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
 }
