176f85e1df9f13d54a81cab246886d392389d0f9
Juan Carlos Salvador <salvaju@oocl.com>
2021-08-04 23:18:41 +0800

    [practice10][Juan Carlos Salvador] feat: should_teacher_introduce_itself_with_which_classes_it_teaches

--- src/main/java/practice10/Teacher.java
+++ src/main/java/practice10/Teacher.java
@@ -1,6 +1,8 @@
 package practice10;
 
+import java.text.MessageFormat;
 import java.util.LinkedList;
+import java.util.stream.Collectors;
 
 public class Teacher extends Person{
 
@@ -13,6 +15,22 @@ public class Teacher extends Person{
 
     public LinkedList<Klass> getClasses() {
         return classes;
+    }
+
+    @Override
+    public String introduce(){
+
+        if (classes != null) {
+            String joinedClasses = classes.stream()
+                    .map(Klass::getNumber)
+                    .map(Object::toString)
+                    .collect(Collectors.joining(", "));
+
+            return MessageFormat.format("{0} I am a Teacher. I teach Class {1}.", super.introduce(), joinedClasses);
+        } else {
+            return MessageFormat.format("{0} I am a Teacher. I teach No Class.", super.introduce());
+        }
+
     }
 
 
