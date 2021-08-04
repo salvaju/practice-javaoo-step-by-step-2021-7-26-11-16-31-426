61ece449c337ce8b4fa4263ce29bae83ac9cca73
Juan Carlos Salvador <salvaju@oocl.com>
2021-08-04 20:25:27 +0800

    [practice07][Juan Carlos Salvador] feat: should_teacher_introduce_itself_with_no_class_teaching

--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -11,6 +11,10 @@ public class Teacher extends Person{
         this.klass = klass;
     }
 
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
     public Klass getKlass() {
         return klass;
     }
