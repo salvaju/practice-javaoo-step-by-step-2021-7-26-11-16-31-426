6a5fdc91c34b9ca543bfc64f4410de97b97f5020
Juan Carlos Salvador <salvaju@oocl.com>
2021-08-04 22:57:27 +0800

    [practice09][Juan Carlos Salvador] feat: should_teacher_introduce_a_student_it_does_not_teach

--- src/test/java/practice09/Practice09Test.java
+++ src/test/java/practice09/Practice09Test.java
@@ -152,13 +152,13 @@ public class Practice09Test {
         assertThat(tom.introduceWith(jerry)).isEqualTo("My name is Tom. I am 21 years old. I am a Teacher. I teach Jerry.");
     }
 
-//    @Test
-//    public void should_teacher_introduce_a_student_it_does_not_teach() throws Exception {
-//        LinkedList<Klass> linkedList = new LinkedList<Klass>();
-//        Klass klass1 = new Klass(1);
-//        linkedList.add(klass1);
-//        Teacher tom = new Teacher(1, "Tom", 21, linkedList);
-//        Student jerry = new Student(1, "Jerry", 8, new Klass(2));
-//        assertThat(tom.introduceWith(jerry)).isEqualTo("My name is Tom. I am 21 years old. I am a Teacher. I don't teach Jerry.");
-//    }
+    @Test
+    public void should_teacher_introduce_a_student_it_does_not_teach() throws Exception {
+        LinkedList<Klass> linkedList = new LinkedList<Klass>();
+        Klass klass1 = new Klass(1);
+        linkedList.add(klass1);
+        Teacher tom = new Teacher(1, "Tom", 21, linkedList);
+        Student jerry = new Student(1, "Jerry", 8, new Klass(2));
+        assertThat(tom.introduceWith(jerry)).isEqualTo("My name is Tom. I am 21 years old. I am a Teacher. I don't teach Jerry.");
+    }
 }
