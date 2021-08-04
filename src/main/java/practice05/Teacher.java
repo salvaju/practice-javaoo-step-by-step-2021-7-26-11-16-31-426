e25d73c47a4f874714fdeea0a4f9ddf7b3bb203a
Juan Carlos Salvador <salvaju@oocl.com>
2021-08-04 18:30:31 +0800

    [practice05][Juan Carlos Salvador] feat: should_teacher_have_name_and_age_and_class

--- src/main/java/practice05/Teacher.java
+++ src/main/java/practice05/Teacher.java
@@ -1,4 +1,15 @@
 package practice05;
 
-public class Teacher {
+public class Teacher extends Person{
+
+    private Integer klass;
+
+    public Teacher(String name, int age, Integer klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Integer getKlass() {
+        return klass;
+    }
 }
