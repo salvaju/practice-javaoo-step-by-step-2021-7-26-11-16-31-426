69b7eaeb185646680122c1cdaaf1a4981af793cb
Juan Carlos Salvador <salvaju@oocl.com>
2021-08-04 18:28:04 +0800

    [practice05][Juan Carlos Salvador] feat: should_student_introduce_with_class

--- src/main/java/practice05/Student.java
+++ src/main/java/practice05/Student.java
@@ -1,5 +1,7 @@
 package practice05;
 
+import java.text.MessageFormat;
+
 public class Student extends Person{
 
     private int klass;
@@ -11,6 +13,11 @@ public class Student extends Person{
 
     public int getKlass() {
         return klass;
+    }
+
+    @Override
+    public String introduce() {
+        return MessageFormat.format("{0} I am a Student. I am at Class {1}.", super.introduce(), klass);
     }
 
 }
--- src/test/java/practice05/Practice05Test.java
+++ src/test/java/practice05/Practice05Test.java
@@ -27,12 +27,12 @@ public class Practice05Test {
         assertThat(tom.getKlass()).isEqualTo(2);
     }
 
-//    @Test
-//    public void should_student_introduce_with_class() throws Exception {
-//        Student tom = new Student("Tom", 21, 2);
-//        assertThat(tom.introduce()).isEqualTo("My name is Tom. I am 21 years old. I am a Student. I am at Class 2.");
-//    }
-//
+    @Test
+    public void should_student_introduce_with_class() throws Exception {
+        Student tom = new Student("Tom", 21, 2);
+        assertThat(tom.introduce()).isEqualTo("My name is Tom. I am 21 years old. I am a Student. I am at Class 2.");
+    }
+
 //    @Test
 //    public void should_teacher_have_name_and_age_and_class() throws Exception {
 //        Teacher tom = new Teacher("Tom", 21, 2);
